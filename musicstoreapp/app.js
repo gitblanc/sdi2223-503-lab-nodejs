1f8f33beb272f9284217e40cde3f6fed2ce411ba
gitblanc <UO285176@uniovi.es>
2023-03-28 20:01:27 +0200

    SDI-2223-503-9.4-Sistema de compras.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -35,8 +35,39 @@ app.set('connectionStrings', url);
 
 const userSessionRouter = require('./routes/userSessionRouter');
 const userAudiosRouter = require('./routes/userAudiosRouter');
+userAudiosRouter.use(function (req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let songId = path.basename(req.originalUrl, '.mp3');
+    let filter = {_id: ObjectId(songId)};
+    songsRepository.findSong(filter, {}).then(song => {
+        if (req.session.user && song.author == req.session.user) {
+            next();
+            return;
+        } else {
+            let filter = {user: req.session.user, songId: ObjectId(songId)};
+            let options = {projection: {_id: 0, songId: 1}};
+            songsRepository.getPurchases(filter, options).then(purchasedIds => {
+                if (purchasedIds !== null && purchasedIds.length > 0) {
+                    next();
+                    return;
+                } else {
+                    res.redirect("/shop");
+                }
+            }).catch(error => {
+                res.redirect("/shop");
+            });
+        }
+    }).catch(error => {
+        res.redirect("/shop");
+    });
+});
+module.exports = userAudiosRouter;
+
 app.use("/songs/add", userSessionRouter);
 app.use("/publications", userSessionRouter);
+app.use("/songs/buy", userSessionRouter);
+app.use("/purchases", userSessionRouter);
 app.use("/audios/", userAudiosRouter);
 app.use("/shop/", userSessionRouter)
 
